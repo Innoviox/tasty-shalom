b44b808b752c99b763fcd9d580807093a4a2ca1c
Simon Chervenak <sarras305@gmail.com>
2016-10-28 17:20:44 -0400

    Add files via upload

---
+++ licenses2.js
@@ -0,0 +1,130 @@
+function licenses(){
+        p1 = window.open('', '', '');
+        p1.document.write(`<!DOCTYPE html>
+<html lang="en">
+<head><title>Licenses for Frameworks utilized by tasty-shalom.tk</title></head>
+  <body>
+Bootstrap License:
+<br>
+    The MIT License (MIT)
+    <br><br>
+    Copyright (c) 2011-2016 Twitter, Inc.<br>
+    Copyright (c) 2011-2016 The Bootstrap Authors<br>
+    <br>
+    Permission is hereby granted, free of charge, to any person obtaining a copy<br>
+    of this software and associated documentation files (the "Software"), to deal<br>
+    in the Software without restriction, including without limitation the rights<br>
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell<br>
+    copies of the Software, and to permit persons to whom the Software is<br>
+    furnished to do so, subject to the following conditions:<br><br>
+    
+    The above copyright notice and this permission notice shall be included in<br>
+    all copies or substantial portions of the Software.<br><br>
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR<br>
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,<br>
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE<br>
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER<br>
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,<br>
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN<br>
+    THE SOFTWARE.<br><br>
+
+Animate License:<br><br>
+
+    The MIT License (MIT)<br><br>
+    
+    Copyright (c) 2016 Daniel Eden<br><br>
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy<br>
+    of this software and associated documentation files (the "Software"), to deal<br>
+    in the Software without restriction, including without limitation the rights<br>
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell<br>
+    copies of the Software, and to permit persons to whom the Software is<br>
+    furnished to do so, subject to the following conditions:<br><br>
+    
+    The above copyright notice and this permission notice shall be included in all<br>
+    copies or substantial portions of the Software.<br><br>
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR<br>
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,<br>
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE<br>
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER<br>
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,<br>
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE<br>
+    SOFTWARE.<br><br>
+
+Tether License:<br><br>
+
+    Copyright (c) 2014-2016 HubSpot, Inc.<br><br>
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:<br><br>
+    
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.<br><br>
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.<br><br>
+
+Hint License<br><br>
+
+    The MIT License (MIT)<br><br>
+    
+    Copyright © 2014 Nicolas Bevacqua<br><br>
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy of<br>
+    this software and associated documentation files (the "Software"), to deal in<br>
+    the Software without restriction, including without limitation the rights to<br>
+    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of<br>
+    the Software, and to permit persons to whom the Software is furnished to do so,<br>
+    subject to the following conditions:<br><br>
+    
+    The above copyright notice and this permission notice shall be included in all<br>
+    copies or substantial portions of the Software.<br><br>
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR<br>
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS<br>
+    FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR<br>
+    COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER<br>
+    IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN<br>
+    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.<br><br>
+
+Drop License:<br><br>
+
+    Copyright (c) 2013 HubSpot, Inc.<br><br>
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+    <br><br>
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+    <br><br>
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+<br><br>
+Foundation-Sites License:       
+<br><br>
+    Copyright (c) 2013-2016 ZURB, inc.
+    <br><br>
+    MIT License
+    <br><br>
+    Permission is hereby granted, free of charge, to any person obtaining<br>
+    a copy of this software and associated documentation files (the<br>
+    "Software"), to deal in the Software without restriction, including<br>
+    without limitation the rights to use, copy, modify, merge, publish,<br>
+    distribute, sublicense, and/or sell copies of the Software, and to<br>
+    permit persons to whom the Software is furnished to do so, subject to<br>
+    the following conditions:<br>
+    <br>
+    The above copyright notice and this permission notice shall be<br>
+    included in all copies or substantial portions of the Software.<br>
+    <br>
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,<br>
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF<br>
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND<br>
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE<br>
+    LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION<br>
+    OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION<br>
+    WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.<br><br>
+
+NOTE: All above licences found on github. <br>
+NOTE: Licenses not on github for the following frameworks:<br>
+    magic
+   </body></html> 
+    `);
+    p1.document.close();
+    }
